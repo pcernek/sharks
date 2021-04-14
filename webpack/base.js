41b7a6d0054b2f5047b1ee9c0da6d79dc35e27e7
pcernek <ptcernek@gmail.com>
2021-04-14 08:47:08 -0700

    Undo env-dependent hitbox alpha

--- webpack/base.js
+++ webpack/base.js
@@ -35,8 +35,7 @@ module.exports = {
       EXPERIMENTAL: JSON.stringify(true),
       PLUGIN_CAMERA3D: JSON.stringify(false),
       PLUGIN_FBINSTANT: JSON.stringify(false),
-      FEATURE_SOUND: JSON.stringify(true),
-      ENV: JSON.stringify('dev')
+      FEATURE_SOUND: JSON.stringify(true)
     }),
     new HtmlWebpackPlugin({
       template: "./index.html"
