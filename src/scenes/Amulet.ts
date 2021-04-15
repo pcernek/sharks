2ed77e5630332169e3775346b6ba68c0ec8fa0db
Paul Cernek <ptcernek@gmail.com>
2021-04-14 23:21:25 -0700

    Format Amulet

--- src/scenes/Amulet.ts
+++ src/scenes/Amulet.ts
@@ -3,17 +3,17 @@ import { Canvas } from '../Canvas';
 
 export class Amulet extends Phaser.Scene {
   constructor() {
-        super({
-          key: 'amulet'
-        });
-    }
+    super({
+      key: 'amulet'
+    });
+  }
 
-    preload() {
-        this.load.image('amulet', '../src/assets/amulet.png');
-    }
+  preload() {
+    this.load.image('amulet', '../src/assets/amulet.png');
+  }
 
-    create() {
-        this.add.image(Canvas.widthPx/2, Canvas.heightPx/2, 'amulet');
-        this.input.on(Phaser.Input.Events.POINTER_DOWN, () => this.scene.switch('lighthouse'))
-    }
+  create() {
+    this.add.image(Canvas.widthPx / 2, Canvas.heightPx / 2, 'amulet');
+    this.input.on(Phaser.Input.Events.POINTER_DOWN, () => this.scene.switch('lighthouse'))
+  }
 }
