81bd2c1f435e20ccbe1626e3671a8a7151a9ed25
Paul Cernek <ptcernek@gmail.com>
2021-04-14 23:41:43 -0700

    Format Lighthouse

--- src/scenes/Lighthouse.js
+++ src/scenes/Lighthouse.js
@@ -1,4 +1,4 @@
-import Phaser from 'phaser';
+import * as Phaser from 'phaser';
 import lighthouse from '../assets/lighthouse-sketch.png'
 import orangeBoat from '../assets/orange-boat-200px.png'
 import jollyRogerBoat from '../assets/jolly-roger-boat-200px.png'
@@ -10,52 +10,52 @@ let CLICK_HANDLER_FILL_COLOR = 0x00ff00
 let CLICK_HANDLER_ALPHA = 0
 
 export class Lighthouse extends Phaser.Scene {
-    constructor() {
-        super({
-          key: 'lighthouse'
-        });
-    }
+  constructor() {
+    super({
+      key: 'lighthouse'
+    });
+  }
 
-    preload() {
-        this.load.image('lighthouse', lighthouse);
-        this.load.image('orangeBoat', orangeBoat)
-        this.load.image('jollyRogerBoat', jollyRogerBoat)
-        this.load.image('whiteBoat', whiteBoat)
-    }
+  preload() {
+    this.load.image('lighthouse', lighthouse);
+    this.load.image('orangeBoat', orangeBoat)
+    this.load.image('jollyRogerBoat', jollyRogerBoat)
+    this.load.image('whiteBoat', whiteBoat)
+  }
 
-    create() {
-        this.add.image(Canvas.widthPx/2, Canvas.heightPx/2, 'lighthouse');
-        const stoneBasinDropZone = this.add.zone(730, 630, 290, 270, 0xff0000, 0.5).setDropZone()
-        new BoatOnShelf(620, 120, 'orangeBoat').addToScene(this, stoneBasinDropZone)
-        new BoatOnShelf(770, 120, 'jollyRogerBoat').addToScene(this, stoneBasinDropZone)
-        new BoatOnShelf(930, 130, 'whiteBoat').addToScene(this, stoneBasinDropZone)
+  create() {
+    this.add.image(Canvas.widthPx / 2, Canvas.heightPx / 2, 'lighthouse');
+    const stoneBasinDropZone = this.add.zone(730, 630, 290, 270).setDropZone()
+    new BoatOnShelf(620, 120, 'orangeBoat').addToScene(this, stoneBasinDropZone)
+    new BoatOnShelf(770, 120, 'jollyRogerBoat').addToScene(this, stoneBasinDropZone)
+    new BoatOnShelf(930, 130, 'whiteBoat').addToScene(this, stoneBasinDropZone)
 
-        this.createClickBox(380, 440, 300, 200, () => {
-          this.scene.switch('battleMap')
-        })
+    this.createClickBox(380, 440, 300, 200, () => {
+      this.scene.switch('battleMap')
+    })
 
-        this.createClickBox(1130, 580, 300, 200, () => {
-          this.scene.switch('amulet')
-        })
-        this.createClickCircle(1200, 280, 130, () => {
-          this.scene.switch('viewFromWindow')
-        })
-        this.createClickCircle(400, 230, 110, () => {
-          this.scene.switch('sharks')
-        })
-    }
+    this.createClickBox(1130, 580, 300, 200, () => {
+      this.scene.switch('amulet')
+    })
+    this.createClickCircle(1200, 280, 130, () => {
+      this.scene.switch('viewFromWindow')
+    })
+    this.createClickCircle(400, 230, 110, () => {
+      this.scene.switch('sharks')
+    })
+  }
 
-    createClickBox(x, y, width, height, onClick) {
-      let clickHandler = this.add.rectangle(x, y, width, height, CLICK_HANDLER_FILL_COLOR, CLICK_HANDLER_ALPHA)
-      clickHandler.setInteractive()
-      clickHandler.on(Phaser.Input.Events.POINTER_DOWN, onClick)
-      return clickHandler
-    }
+  createClickBox(x, y, width, height, onClick) {
+    let clickHandler = this.add.rectangle(x, y, width, height, CLICK_HANDLER_FILL_COLOR, CLICK_HANDLER_ALPHA)
+    clickHandler.setInteractive()
+    clickHandler.on(Phaser.Input.Events.POINTER_DOWN, onClick)
+    return clickHandler
+  }
 
-    createClickCircle(x, y, radius, onClick) {
-      let clickHandler = this.add.circle(x, y, radius, CLICK_HANDLER_FILL_COLOR, CLICK_HANDLER_ALPHA)
-      clickHandler.setInteractive()
-      clickHandler.on(Phaser.Input.Events.POINTER_DOWN, onClick)
-      return clickHandler
-    }
+  createClickCircle(x, y, radius, onClick) {
+    let clickHandler = this.add.circle(x, y, radius, CLICK_HANDLER_FILL_COLOR, CLICK_HANDLER_ALPHA)
+    clickHandler.setInteractive()
+    clickHandler.on(Phaser.Input.Events.POINTER_DOWN, onClick)
+    return clickHandler
+  }
 }
